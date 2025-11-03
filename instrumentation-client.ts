2d667e67086b7a35a525510e9702f61901a6fbd4
joseph sunday <josephluckysunday@gmail.com>
2025-11-03 22:22:28 +0000

    Implement PostHog

---
+++ instrumentation-client.ts
@@ -0,0 +1,11 @@
+import posthog from 'posthog-js'
+
+// Automatically initializes PostHog in your Next.js app
+posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+  api_host: '/ingest',
+  ui_host: 'https://us.posthog.com',
+  defaults: '2025-05-24',
+  capture_exceptions: true, // Enables capturing exceptions via Error Tracking
+  debug: process.env.NODE_ENV === 'development',
+});
+ 
